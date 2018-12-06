99eb75c2a2406cf6bc6bb01a1751618ddd6794d1
jgbaldwinbrown <jgbaldwinbrown@gmail.com>
2018-12-06 10:41:03 -0700

    ready for first big test

---
+++ firstsims.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+e=0
+
+print "all: datadir/alldone.txt"
+print "datadir/madedir.txt:"
+print "	mkdir -p `dirname $@`"
+print "	touch $@"
+
+names = []
+for n in [1000,10000]:
+    for f in [1000, 10000]:
+        for g in [100, 1000]:
+            for s in [.1, 1]:
+                for p in [0, 0.01, 0.1]:
+                    for v in [.1, 1]:
+                        for q in [.8, 1, 1.4]:
+                            for a in [0, 1, 2, 4]:
+                                for t in [0, .001, .01, .1]:
+                                    for i in xrange(50):
+                                        name = "datadir/sim_n%d_f%d_g%d_s%f_p%f_v%f_q%f_a%d_t%f_e%f_i%f.txt.gz" % (n,f,g,s,p,v,q,a,t,e,i)
+                                        command = "./simple_test  -n %d -f %d -g %d -s %f -p %f -v %f -q %f -a %d -t %f -e %d"  % (n,f,g,s,p,v,q,a,t,e)
+                                        print name + ": datadir/madedir.txt"
+                                        print "	" + command + " | gzip -c > $@"
+                                        e += 1
+                                        names.append(name)
+
+print "datadir/alldone.txt: " + " ".join(names)
+            
+#usage: ./simple_test [options]
+#
+#argument   description
+#-n         Population size (default=10)
+#-m         Mean initial body size (default=10)
+#-s         Initial body size standard deviation (default=1)
+#-p         Initial probability of carnivore (default = 0)
+#-v         Standard deviation body size change (default = 1)
+#-f         Total food (default=120)
+#-q         Ratio of carnivore to herbivore for eating (default = 1)
+#-a         Number of tries carnivore has to eat (default = 2)
+#-t         Probability of carnivore-herbivore transition (default=0)
+#-g         Number of generations (default = 5)
+#-h         Random seed unsigned integer (default = 0)
+#-h         Print this help text
--- simple_test.c
+++ simple_test.c
@@ -3,8 +3,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
 
 int main (int argc, char **argv){
+    char usage_txt[] =
+"usage: %s [options]\n"
+"\n"
+"argument   description\n"
+"-n         Population size (default=10)\n"
+"-m         Mean initial body size (default=10)\n"
+"-s         Initial body size standard deviation (default=1)\n"
+"-p         Initial probability of carnivore (default = 0)\n"
+"-v         Standard deviation body size change (default = 1)\n"
+"-f         Total food (default=120)\n"
+"-q         Ratio of carnivore to herbivore for eating (default = 1)\n"
+"-a         Number of tries carnivore has to eat (default = 2)\n"
+"-t         Probability of carnivore-herbivore transition (default=0)\n"
+"-g         Number of generations (default = 5)\n"
+"-h         Random seed unsigned integer (default = 0)\n"
+"-h         Print this help text\n";
     /*int index;*/
     char *n_txt;
     char *m_txt;
@@ -12,12 +30,34 @@ int main (int argc, char **argv){
     char *p_txt;
     char *g_txt;
     char *e_txt;
+    char *v_txt;
+    char *f_txt;
+    char *q_txt;
+    char *a_txt;
+    char *t_txt;
+    /* population size */
     long long n = 10;
+    /* mean initial body size */
     double m = 10.0;
+    /* initial body size standard deviation */
     double s = 1.0;
+    /* initial probability of carnivore */
     double p = 0.0;
+    /* stdev change upon reproduction */
+    double v = 1.0;
+    /* total food available */
+    double f = 120;
+    /* multiplicative amount that carn must be larger than herb to eat it */
+    double q = 1.0;
+    /* number of tries that carnivore has to eat */
+    long long a = 2;
+    /* probability of carn-herb transition */
+    double t = 0.0;
+    /* number of generations */
     long long g = 5;
+    /* random seed */
     unsigned long int e = 0;
+    long long gens_survived = 0;
 
     int c;
     opterr = 0;
@@ -25,7 +65,7 @@ int main (int argc, char **argv){
     full_exp *exp;
 
     /* parse all arguments */
-    while ((c = getopt (argc, argv, "nmspge:")) != -1)
+    while ((c = getopt (argc, argv, "n:m:s:p:g:e:v:f:q:a:t:h")) != -1)
         switch (c)
             {
             case 'n':
@@ -41,7 +81,7 @@ int main (int argc, char **argv){
                 sscanf(s_txt, "%lf", &s);
                 break;
             case 'p':
-                s_txt = optarg;
+                p_txt = optarg;
                 sscanf(p_txt, "%lf", &p);
                 break;
             case 'g':
@@ -52,8 +92,42 @@ int main (int argc, char **argv){
                 e_txt = optarg;
                 sscanf(e_txt, "%lu", &e);
                 break;
+            case 'v':
+                v_txt = optarg;
+                sscanf(v_txt, "%lf", &v);
+                break;
+            case 'f':
+                f_txt = optarg;
+                sscanf(f_txt, "%lf", &f);
+                break;
+            case 'q':
+                q_txt = optarg;
+                sscanf(q_txt, "%lf", &q);
+                break;
+            case 'a':
+                a_txt = optarg;
+                sscanf(a_txt, "%lld", &a);
+                break;
+            case 't':
+                t_txt = optarg;
+                sscanf(t_txt, "%lf", &t);
+                break;
+            case 'h':
+                printf(usage_txt, argv[0]);
+                exit(0);
+                break;
             case '?':
-                if (optopt == 'n' || optopt == 'm' || optopt == 's' || optopt == 'g' || optopt == 'p' || optopt == 'e'){
+                if (optopt == 'n' || 
+                    optopt == 'm' || 
+                    optopt == 's' || 
+                    optopt == 'g' || 
+                    optopt == 'p' || 
+                    optopt == 'e' || 
+                    optopt == 'v' || 
+                    optopt == 'f' || 
+                    optopt == 'q' || 
+                    optopt == 'a' || 
+                    optopt == 't'){
                     fprintf (stderr, "Option -%c requires an argument.\n", optopt);
                 } else if (isprint (optopt)){
                     fprintf (stderr, "Unknown option `-%c'.\n", optopt);
@@ -71,11 +145,18 @@ int main (int argc, char **argv){
     gsl_rng_set (myrng, e);
     
     /*set up pop to evolve*/
+    /*
     exp = init_full_exp(n, g);
+    rinit_full_exp(exp, m, s, p, myrng);
+    */
+    exp = init_full_exp(n,g);
     rinit_full_exp(exp, m, s, p, myrng);
 
     /*do the experimental evolution*/
     /*evo_all_gens(exp);*/
+    gens_survived = run_full_exp(exp, v, t, myrng, f, a, q);
+    printf("Generations survived: %lld\n", gens_survived);
+    prexp(exp);
 
     /*free the main data structure*/
     free_exp(exp);
@@ -90,3 +171,24 @@ int main (int argc, char **argv){
     return 0;
 }
 
+/*
+int main(){
+    full_exp *a;
+    double food = 80000;
+    long long gens_survived = 0;
+    gsl_rng * myrng = gsl_rng_alloc (gsl_rng_taus);
+    a = init_full_exp(10000,30);
+    rinit_full_exp(a, 5, 1, .1, myrng);
+
+    printf("\n");
+
+    gens_survived = run_full_exp(a, .2, .1, myrng, food, 500, 1.1);
+
+    printf("Generations survived: %lld\n", gens_survived);
+    prexp(a);
+    
+    gsl_rng_free(myrng);
+    free_exp(a);
+    return(0);
+}
+*/
